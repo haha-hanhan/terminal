f12c0b0d8d959a11721aca8dec04aab56e6a3feb
Dustin L. Howett <duhowett@microsoft.com>
2020-06-18 23:52:15 -0700

    Some tests are now invalid

--- src/cascadia/LocalTests_TerminalApp/SettingsTests.cpp
+++ src/cascadia/LocalTests_TerminalApp/SettingsTests.cpp
@@ -1435,10 +1435,6 @@ namespace TerminalAppLocalTests
                 {
                     "name": "profile3",
                     "closeOnExit": null
-                },
-                {
-                    "name": "profile4",
-                    "closeOnExit": { "clearly": "not a string" }
                 }
             ]
         })" };
@@ -1453,7 +1449,6 @@ namespace TerminalAppLocalTests
 
         // Unknown modes parse as "Graceful"
         VERIFY_ARE_EQUAL(CloseOnExitMode::Graceful, settings._profiles[3].GetCloseOnExitMode());
-        VERIFY_ARE_EQUAL(CloseOnExitMode::Graceful, settings._profiles[4].GetCloseOnExitMode());
     }
     void SettingsTests::TestCloseOnExitCompatibilityShim()
     {
